77e59836de1636df141da31ef9860f2247b2d6a9
Rafael Malespin <leoblade24@ufl.edu>
2019-10-02 15:28:01 -0400

    Saved from step 7

---
+++ Bootcamp3/server/config/config.js
@@ -0,0 +1,11 @@
+
+module.exports ={
+        db: {
+                uri: 'mongodb+srv://Rafael:Rjm4508@cluster0-cwbxq.mongodb.net/UFmap?retryWrites=true&w=majority',
+        },
+	openCage:{
+		key:'beba44e3dc8a4136926384f61c20706d'
+	},
+	port:8080
+};
+
--- Bootcamp3/server/controllers/listings.server.controller.js
+++ Bootcamp3/server/controllers/listings.server.controller.js
@@ -1,9 +1,9 @@
 
 /* Dependencies */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Listing = require('../models/listings.server.model.js'),
     coordinates = require('./coordinates.server.controller.js');
-    
+
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
   On an error you should send a 404 status code, as well as the error message. 
@@ -13,7 +13,7 @@ var mongoose = require('mongoose'),
   https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
   or
   https://medium.com/@dinyangetoh/how-to-build-simple-restful-api-with-nodejs-expressjs-and-mongodb-99348012925d
-  
+
 
   If you are looking for more understanding of exports and export modules - 
   https://www.sitepoint.com/understanding-module-exports-exports-node-js/
@@ -30,11 +30,11 @@ exports.create = function(req, res) {
   /* save the coordinates (located in req.results if there is an address property) */
   if(req.results) {
     listing.coordinates = {
-      latitude: req.results.lat, 
+      latitude: req.results.lat,
       longitude: req.results.lng
     };
   }
- 
+
   /* Then save the listing */
   listing.save(function(err) {
     if(err) {
@@ -58,24 +58,54 @@ exports.update = function(req, res) {
   var listing = req.listing;
 
   /* Replace the listings's properties with the new properties found in req.body */
- 
+	listing.code=req.body.code;
+	listing.name-req.body.name;
+	listing.address=req.body.address;
   /*save the coordinates (located in req.results if there is an address property) */
- 
+ 	if(req.results){
+		listing.coordinates={
+			latitude:req.results.lat,
+			longitude: req.results.lng
+		};
+	}
   /* Save the listing */
+	listing.save(function(err){
+		if(err){
+			res.status(400).send(err);
+		}
+		else{
+			res.json(listing);
+			console.log(listing);
+		}
+	});
 
 };
 
 /* Delete a listing */
 exports.delete = function(req, res) {
   var listing = req.listing;
-
   /* Add your code to remove the listins */
-
+	listing.remove(function(err){
+		if(err){
+			res.status(400).send(err);
+		}
+		else{
+			res.json(listing);
+			console.log(listing);
+		}
+	});
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Add your code */
+	Listing.find()
+		.then(listings=>{
+			res.send(listings);
+		}).catch(err=>{
+			res.status(400).send(err)
+		});
+
 };
 
 /* 
